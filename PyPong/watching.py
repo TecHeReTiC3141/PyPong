4608510c804460013936be2b2015af8b648efa31
TecHeReTiC <talex73@mail.ru>
2022-02-14 00:48:25 +0500

    following algorithm is ready I hope.

--- PyPong/watching.py
+++ PyPong/watching.py
@@ -1,18 +1,43 @@
 import pygame
 import pygame as pg
 from random import randint
-from math import asin, sin, pi, sqrt
+from math import asin, acos, sin, pi, sqrt
 
 pg.init()
 
 display_width, display_height = 800, 600
 display = pg.display.set_mode((display_width, display_height))
 
-def from_rads_to_deg(r):
-    return r * 180 / pi
+points_coords = pygame.USEREVENT + 2
+pygame.time.set_timer(points_coords, 2000)
 
-def dec_dist(x1, y1, x2, y2):
-    return sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+
+class Math:
+
+    @staticmethod
+    def from_rads_to_deg(r):
+        return r * 180 / pi
+
+    @staticmethod
+    def dec_dist(x1, y1, x2, y2):
+        return sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+
+    @staticmethod
+    def calc_angle(abc, ord, rad, quarter):
+
+        if quarter is None:
+            return 1, 0, 0, 0
+        if quarter % 2:
+            ord /= rad
+            meas = Math.from_rads_to_deg(asin(min(1., ord)))
+        else:
+            abc /= rad
+            meas = 90 - Math.from_rads_to_deg(acos(min(1., abc)))
+
+        angle = meas + (point.quarter - 1) * 90
+
+        return abc, ord, meas, angle
+
 
 class Point:
 
@@ -22,11 +47,12 @@ class Point:
         self.radius = radius
         self.color = color
         self.speed = speed
+        self.quarter = None
 
     def draw(self, sc: pg.Surface):
         pg.draw.circle(sc, self.color, (self.x, self.y), self.radius)
 
-    def move(self, rad: int, center: tuple):
+    def move(self, rads: int, center: tuple):
         keys = pg.key.get_pressed()
         # if keys[pg.K_UP]:
         #     self.y -= self.speed
@@ -37,12 +63,44 @@ class Point:
         # if keys[pg.K_RIGHT]:
         #     self.x += self.speed
         if keys[pg.K_DOWN]:
-            self.y += self.speed
-            self.x = center[0] + sqrt(abs(rad ** 2 - (self.y - center[1]) ** 2))
+            if self.x >= center[0] and self.y >= center[1]:
+                self.quarter = 4
+            if self.x <= center[0] and self.y >= center[1]:
+                self.quarter = 3
+            if self.x <= center[0] and self.y <= center[1]:
+                self.quarter = 2
+            if self.x >= center[0] and self.y <= center[1]:
+                self.quarter = 1
+            sec_cat = rads ** 2 - (self.y - center[1]) ** 2
+            if self.quarter in [1, 4]:
+                self.y += self.speed
+                self.x = round(center[0] + sqrt(abs(sec_cat)))
+            else:
+                self.y -= self.speed
+                self.x = round(center[0] - sqrt(abs(sec_cat)))
+
+
+
 
         elif keys[pg.K_UP]:
-            self.y -= self.speed
-            self.x = center[0] + sqrt(abs(rad ** 2 - (self.y - center[1]) ** 2))
+            if self.x >= center[0] and self.y <= center[1]:
+                self.quarter = 1
+            if self.x <= center[0] and self.y <= center[1]:
+                self.quarter = 2
+            if self.x <= center[0] and self.y >= center[1]:
+                self.quarter = 3
+            if self.x >= center[0] and self.y >= center[1]:
+                self.quarter = 4
+
+            sec_cat = rads ** 2 - (self.y - center[1]) ** 2
+            if self.quarter in [1, 4]:
+                self.y -= self.speed
+                self.x = round(center[0] + sqrt(abs(sec_cat)))
+            else:
+                self.y += self.speed
+                self.x = round(center[0] - sqrt(abs(sec_cat)))
+        return # add return
+
 
 class image:
 
@@ -71,7 +129,10 @@ class image:
         pg.draw.line(sc, '#FF0000', (p_x, p_y), (os_x, os_y), 3)
         pg.draw.line(sc, '#00FF00', (c_x, c_y), (os_x, os_y), 3)
         pg.draw.line(sc, '#0000FF', (c_x, c_y), (p_x, p_y), 3)
-        return
+        pg.draw.line(sc, '#00FFFF', (p_x, c_y), (p_x, p_y), 3)
+        pg.draw.line(sc, '#FFFF00', (p_x, os_y), (os_x, os_y), 3)
+
+        return (abs(p_x - c_x), abs(p_y - c_y))
 
     def get_surf_center(self) -> tuple[int, int]:
         return self.x + self.image.get_width() // 2, self.y + self.image.get_height() // 2
@@ -85,7 +146,6 @@ class image:
     #     return prot / pril, self.angle
 
 
-
 hank = pg.transform.flip(pg.transform.scale(pg.image.load('./pypong_images/shank.jpg'),
                                             (225, 235)), True, False)
 hank_image = image(hank.get_width() * 2, hank.get_height() * 2, display_width // 3, display_height // 4, hank)
@@ -93,7 +153,7 @@ rad = hank_image.width // 2
 img_center = hank_image.get_surf_center()
 
 point = Point(hank_image.get_surf_center()[0] + hank_image.width // 2,
-              hank_image.get_surf_center()[1], 8, '#c2200c')
+              hank_image.get_surf_center()[1], 8, '#c2200c', speed=1)
 
 clock = pg.time.Clock()
 
@@ -104,10 +164,16 @@ while True:
         if event.type == pg.QUIT:
             pg.quit()
 
-    display.fill((240, 240, 240))
-    hank_image.draw(display, (point.x, point.y))
-    point.draw(display)
+        elif event.type == points_coords:
+            print(point.quarter, point.x - img_center[0], point.y - img_center[1], rad, rad ** 2 - (point.y - img_center[1]) ** 2)
 
+    display.fill((240, 240, 240))
+
+
+    ord, abc, meas, angle = Math.calc_angle(*hank_image.draw(display, (point.x, point.y)),
+                                  rad, point.quarter)
+
+    point.draw(display)
     pg.draw.circle(display, "#BB0000", hank_image.get_surf_center(), 3)
 
     pygame.draw.circle(display, '#000000', hank_image.get_surf_center(),
@@ -119,5 +185,8 @@ while True:
 
     clock.tick(60)
     tick += 1
-    hank_image.rotated_image = pg.transform.rotate(hank_image.image, -angle)
-    angle += 1
+    hank_image.rotated_image = pg.transform.rotate(hank_image.image, angle)
+
+    if not tick % 120:
+        print(abc, ord, meas, angle)
+
